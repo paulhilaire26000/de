2e02dc6507911ebd45805590c19f3a954854469e
paulhilaire26000 <124454355+paulhilaire26000@users.noreply.github.com>
2023-02-11 14:44:26 +0100

    marche

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showNumber(1)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showNumber(randint(1, 3))
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . . # .
+            . . # . .
+            . # # # .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showNumber(randint(1, 6))
+    }
 })
 basic.forever(function () {
 	
